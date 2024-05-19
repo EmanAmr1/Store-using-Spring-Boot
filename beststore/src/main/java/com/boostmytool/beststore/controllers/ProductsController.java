7b53ff6aa81486efc21640ed7b73f24c32babb92
EmanAmr1 <emanamr543@gmail.com>
2024-05-19 14:16:48 +0300

    save the image of the new product

--- beststore/src/main/java/com/boostmytool/beststore/controllers/ProductsController.java
+++ beststore/src/main/java/com/boostmytool/beststore/controllers/ProductsController.java
@@ -12,7 +12,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -85,13 +92,54 @@ public class ProductsController {
         //check imageFile manually that it is notEmpty
 
         if(productDto.getImageFile().isEmpty()){
-            result.addError(new FieldError("productDto" ,"imageFile" ,"the file is requires"));
+            result.addError(new FieldError("productDto" ,"imageFile" ,"the file is required"));
         }
 
         if(result.hasErrors()){
             return "products/createProduct";
         }
 
+
+        //save image file
+
+        MultipartFile image = productDto.getImageFile(); //Read image from dto
+        Date createdAt = new Date();  //read the date
+        String storageFileName =image.getOriginalFilename(); //create unique fileName to this image
+
+        try{
+            String uploadDir ="public/images/"; //folder to save image
+            Path uploadPath = Paths.get(uploadDir); //obj of type path
+
+            if(!Files.exists(uploadPath)){
+                Files.createDirectories(uploadPath);
+            }
+            try (InputStream inputStream=image.getInputStream()){  //obj
+                Files.copy(inputStream,Paths.get(uploadDir + storageFileName), StandardCopyOption.REPLACE_EXISTING);
+            }
+        }catch (Exception ex){
+            System.out.println("Exception: "+ex.getMessage());
+        }
+
+        //to save product in the db
+
+        Product p = new Product();  //take obj from product to save the  new product on it
+        p.setName(productDto.getName());
+        p.setBrand(productDto.getBrand());
+        p.setCategory(productDto.getCategory());
+        p.setPrice(productDto.getPrice());
+        p.setDescription(productDto.getDescription());
+        p.setCreatedAt(createdAt);   // the current date
+        p.setImageFileName(storageFileName); //the uploaded file
+
+        repo.save(p);
+
+
+
+
         return "redirect:/products";
     }
+
+
+
+
 }
